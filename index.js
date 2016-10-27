4c890354e7028051d099a09e26cd59d89d1466cf
Ryan Thames <rthames62@gmail.com>
2016-10-27 16:55:14 -0500

    db setup

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const json = require('body-parser').json;
+const session = require('express-session');
+const passport = require('passport');
+const FacebookStrategy = require('passport-facebook').Strategy;
+const config = require('./config.js');
+const mongoose = require("mongoose");
+const mongoUri = config.mongoUri.uri;
+
+const app = express();
+const port = 8888;
+
+app.use(express.static(`${__dirname}/public`));
+app.use(json({limit: '50mb'}));
+// app.use(cors());
+// require("./masterRoutes.js")(app);
+mongoose.connect(mongoUri);
+mongoose.connection.once("open", function(){console.log("connected to dnd db");});
+app.use(session({secret : config.mySecrets.secret}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new FacebookStrategy({
+  clientID : config.facebook.clientID,
+  clientSecret : config.facebook.secret,
+  callbackURL : config.facebook.cbUrl,
+  profileFields : ["id", "name", "gender", "link", "email", "location", "cover", "friends", "friendlists", "picture"]
+}, function(token, refreshToken, profile, done){
+  return done(null, profile);
+}));
+
+function loggedIn(req, res, next){
+  if(req.user){
+    next();
+  } else {
+    res.redirect("/");
+  }
+}
+
+
+app.get("/auth/facebook", passport.authenticate("facebook"));
+app.get("/auth/facebook/callback", passport.authenticate("facebook", {
+  successRedirect : "/#",
+  failureRedirect : "/"
+}));
+
+passport.serializeUser(function(user, done){
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done){
+  done(null, user);
+});
+
+app.get("/api/facebook", loggedIn, function(req, res, next){
+  res.send(req.user);
+})
+
+app.listen(port, function(){console.log(`Listening on port ${port}`);})
---
+++ masterRoutes.js
@@ -0,0 +1,15 @@
+const userRoutes = require("./features/users/userRoutes.js");
+const dundeonRoutes = require("./features/dungeons/dungeonRoutes.js");
+const gameRoutes = require("./features/game/gameRoutes.js");
+const itemRoutes = require("./features/items/itemRoutes.js");
+const monsterRoutes = require("./features/monsters/monsterRoutes.js");
+const spellRoutes = require("./features/spells/spellRoutes.js");
+
+module.exports = function(app){
+  userRoutes(app);
+  dundeonRoutes(app);
+  gameRoutes(app);
+  itemRoutes(app);
+  monsterRoutes(app);
+  spellRoutes(app);
+}
