2927a0ae0bd35d0dc14a828ebfe6afb95f614fde
Ian Robbins <iedwardrobbins@gmail.com>
2016-11-03 12:02:57 -0500

    finished populating db with classes, races, items.

--- features/races/Race.js
+++ features/races/Race.js
@@ -4,11 +4,13 @@ const Race = new mongoose.Schema({
     name: {type: String, required: true, uinque: true, trim: true}
     , traitBonus: {
         plus: [{
-            statName: {type: String, trim: true}
+            _id: false
+            , statName: {type: String, trim: true}
             , plusAmt: {type: Number}
         }]
         , minus: [{
-            statName: {type: String, trim: true}
+            _id: false
+            , statName: {type: String, trim: true}
             , minusAmt: {type: Number}
         }]
         , desc: {type: String, trim: true}
@@ -22,21 +24,15 @@ const Race = new mongoose.Schema({
     , baseSpeed: {type: Number, trim: true}
     , languages: [{type: String, trim: true}]
     , racialTraits: [{
-        traitType: {type: String, trim: true}
+        _id: false
+        , traitType: {type: String, trim: true}
         , traitName: {type: String, trim: true}
         , targetStats: [{type: String, trim: true}]
         , bonusNum: {type: Number}
         , desc: {type: String, trim: true}
     }]
     , favoredClasses: [{type: mongoose.Schema.Types.ObjectId, ref: "CharClass"}]
-    , description: {
-        general: {type: String, required: true, trim: true}
-        , physicalDescription: {type: String, required: true, trim: true}
-        , society: {type: String, required: true, trim: true}
-        , relations: {type: String, required: true, trim: true}
-        , alignment: {type: String, required: true, trim: true}
-        , adventures: {type: String, required: true, trim: true}
-    }
+    , description: {type: String, required: true, trim: true}
 });
 
 module.exports = mongoose.model("Race", Race);
