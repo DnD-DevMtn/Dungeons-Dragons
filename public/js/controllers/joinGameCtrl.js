f2841910e436be5ee62a3123b2e702907080e0df
Ian Robbins <iedwardrobbins@gmail.com>
2016-11-06 16:02:27 -0600

    changed the promise in the joinGame controller

--- public/js/controllers/joinGameCtrl.js
+++ public/js/controllers/joinGameCtrl.js
@@ -2,13 +2,19 @@ export default function(joinService, userService){
 
     const joinGame = this;
 
-    joinGame.userService.user;            // gets the current user from the userService
+    joinGame.currentUser = userService.user;            // gets the current user from the userService
 
+    joinGame.campaigns = [];
     joinGame.getCampaigns = () => {
         joinService.getCampaigns().then(campaigns => {
+            for(let i = 0; i < campaigns; i++){
+                for(let j = 0; j < campaigns[i].players.length; j++){
+                    if(campaigns[i].players[j].facebookId === currentUser.facebookId){
+                        campaigns.splice(i, 1);
+                    }
+                }
+            }
             joinGame.campaigns = campaigns;
-
-
         });
     }
 
