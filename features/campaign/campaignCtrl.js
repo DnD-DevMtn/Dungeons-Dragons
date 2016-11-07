23ba0ad9539e94681950322acf4d4381b4a70fe4
Ian Robbins <iedwardrobbins@gmail.com>
2016-11-06 22:32:03 -0600

    campaigns now post to users after cahracter creation

--- features/campaign/campaignCtrl.js
+++ features/campaign/campaignCtrl.js
@@ -19,13 +19,12 @@ module.exports = {
     , postCampaign(req, res){
         new Campaign(req.body.campaign).save((err, campaign) => {
             if(err) {
-              return res.status(500).json(err);
+                return res.status(500).json(err);
             } else {
-              User.findOneAndUpdate({facebookId: req.body.facebookId}, {$push: {campaigns: campaign._id}}, (err, user) => {
-              })
-              return res.status(201).json(campaign);
+                User.findOneAndUpdate({facebookId: req.body.facebookId}, {$push: {campaigns: campaign._id}}, (err, user) => {
+            })
+                return res.status(201).json(campaign);
             }
-            //return (err) ? res.status(500).json(err) : res.status(201).json(campaign);
         });
     }
 
@@ -37,13 +36,15 @@ module.exports = {
     }
 
     , addPlayer(req, res){
-        console.log("ADDING PLAYER", req.body);
-        console.log("THIS IS THE ID", req.params.id);
         Campaign.findByIdAndUpdate(req.params.id
                                    , {$push: {players: {facebookId: req.body.facebookId, character: req.body.character}}}
                                    , (err, campaign) => {
-                                       console.log(campaign);
-            return (err) ? res.status(500).json(err) : res.status(200).json(campaign);
+            if(err){
+                return res.status(500).json(err);
+            } else {
+                User.findOneAndUpdate({facebookId: req.body.facebookId}, {$push: {campaigns: req.params.id}}, (err, user) => {});
+                return res.status(200).json(campaign);
+            }
         });
     }
 
