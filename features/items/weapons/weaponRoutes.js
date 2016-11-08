1cfffabb3ff6dcd2044dd284f3a4323a37e7dcc4
Dominique Clarke <dominiquedclarke@gmail.com>
2016-11-08 16:21:07 -0600

    added style tweeks to lobby and added inventory data

--- features/items/weapons/weaponRoutes.js
+++ features/items/weapons/weaponRoutes.js
@@ -1,12 +1,12 @@
 const weaponCtrl = require("./weaponCtrl");
 
 module.exports = app => {
+  
+    app.route("/api/weapons/:id")
+          .get(weaponCtrl.getWeaponById);
 
     app.route("/api/weapons")
         .get(weaponCtrl.getWeapons)
         .post(weaponCtrl.postWeapon);
-
-    app.route("/api/weapons/:id")
-        .get(weaponCtrl.getWeaponById);
 
 }
