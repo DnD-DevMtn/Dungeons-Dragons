efc53f9793fbbe6db7a9a1e8e7f2d2d2ddc7f9fe
jaebum.yoo <jaebum.yoo@gmail.com>
2016-11-08 18:19:50 -0600

    Map Builder Save To Mongo

--- features/dungeons/dungeonRoutes.js
+++ features/dungeons/dungeonRoutes.js
@@ -1,12 +1,10 @@
 const dungeonCtrl = require("./dungeonCtrl");
 
 module.exports = app => {
+  app.route("/api/dungeons/:id")
+    .get(dungeonCtrl.getDungeonById);
 
-    app.route("/api/dungeons")
-        .get(dungeonCtrl.getDungeons)
-        .post(dungeonCtrl.postDungeon);
-
-    app.route("/api/dungeons/:id")
-        .get(dungeonCtrl.getDungeonById);
-
+  app.route("/api/dungeons")
+    .get(dungeonCtrl.getDungeons)
+    .post(dungeonCtrl.postDungeon);
 }
