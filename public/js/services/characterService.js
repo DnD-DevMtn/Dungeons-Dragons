2eb8eb33e4dea191d8ccfa0fc14f1ca727901620
Dominique Clarke <dominiquedclarke@gmail.com>
2016-11-08 19:06:03 -0600

    added http request for baseAttack

--- public/js/services/characterService.js
+++ public/js/services/characterService.js
@@ -32,25 +32,30 @@ function characterService($http, $state, userService) {
     currentCharacter.baseStats.int = int;
     currentCharacter.baseStats.wis = wis;
     currentCharacter.baseStats.cha = cha;
+    currentCharacter.money.gold = 50;
     if(currentCharacter.classType === "Fighter"){
+      const classId = "5818cf737e79500420928e5d";
       currentCharacter.weapons = ["581a5e0aaba7c616582a7afe"];
       currentCharacter.armor = ["581a653dfc9a0f0b2a47a842"];
       currentCharacter.gear = ["581a5a142709d90a714c18e5"];
       currentCharacter.sprite = "FIGHTER";
       currentCharacter.hp = (10 * currentCharacter.level) + (Math.floor(con - 10) / 2);
     } else if(currentCharacter.classType === "Rogue"){
+      const classId = "5818cff07e79500420928e5e";
       currentCharacter.weapons = ["5817d7626b784f1c14e36f5e", "581a47ef959c9233f0f9eaac", "581a5fff89e4592710b834dd"];
       currentCharacter.armor = ["581a51762709d90a714c18d0"];
       currentCharacter.gear = ["581a5a142709d90a714c18e5"];
       currentCharacter.sprite = "ROGUE";
       currentCharacter.hp = (6 * currentCharacter.level) + (Math.floor(con - 10) / 2);
     } else if(currentCharacter.classType === "Sorcerer"){
+      const classId = "581b56450be6891a5843f1c1";
       currentCharacter.weapons = ["581a54c43045432ae855bb55"];
       currentCharacter.armor = ["581a652cfc9a0f0b2a47a841"];
       currentCharacter.gear = ["581a5a142709d90a714c18e5"];
       currentCharacter.sprite = "SORCERER";
       currentCharacter.hp = (6 * currentCharacter.level) + (Math.floor(con - 10) / 2);
     } else if(currentCharacter.classType === "Cleric"){
+      const classId = "581b5cc00be6891a5843f1c2";
       currentCharacter.weapons = ["581a50f6c97475280cf88209"];
       currentCharacter.armor = ["581a6549fc9a0f0b2a47a844"];
       currentCharacter.gear = ["581a5a142709d90a714c18e5"];
@@ -70,13 +75,22 @@ function characterService($http, $state, userService) {
     } else if(currentCharacter.race === "Half Orc"){
       currentCharacter.speed = 6;
     }
-    $http.put(`/api/users/${currentUser}`, currentCharacter);
-    $http.put(`/api/campaigns/join/${room}`, {facebookId: userService.user.facebookId, character: currentCharacter});
-    userService.user.character = currentCharacter;
-    $state.go("lobby", {campaign: campaign, gameId: room, userChar: currentCharacter});
+    getBaseAttack(classId).then(results => {
+      console.log('here are the results', results);
+      $http.put(`/api/users/${currentUser}`, currentCharacter);
+      $http.put(`/api/campaigns/join/${room}`, {facebookId: userService.user.facebookId, character: currentCharacter});
+      userService.user.character = currentCharacter;
+    }
+      //$state.go("lobby", {campaign: campaign, gameId: room, userChar: currentCharacter});
   }
 
   this.getUser();
+
+  function getBaseAttack(classId) {
+    return $http.get(`/api/classes/${classId}`).then(class => {
+      console.log(class);
+    })
+  }
 }
 
 module.exports = characterService;
