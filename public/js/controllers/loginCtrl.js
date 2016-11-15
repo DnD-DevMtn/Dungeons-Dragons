d718441fb11244f2f09a8e319a5b8879ffc14439
Dominique Clarke <dominiquedclarke@gmail.com>
2016-11-15 13:07:15 -0600

    tweeks to parallax

--- public/js/controllers/loginCtrl.js
+++ public/js/controllers/loginCtrl.js
@@ -11,7 +11,7 @@ export default function() {
   login.appendChild(renderer.view);
 
 
-  let far, mid, close;
+  let farthest, far, mid, close;
 
   PIXI.loader
     .add("../../assets/login-sprites.json")
@@ -21,7 +21,8 @@ export default function() {
   function setup() {
 
             close = new PIXI.extras.TilingSprite(
-              resources["../../assets/login-sprites.json"].textures["town.png"],512, 512
+              resources["../../assets/login-sprites.json"].textures["town.png"],
+              512, 512
             );
             mid = new PIXI.extras.TilingSprite(
               resources["../../assets/login-sprites.json"].textures["church-castle.png"],
@@ -31,19 +32,31 @@ export default function() {
               resources["../../assets/login-sprites.json"].textures["castle1.png"],
               512, 512
             );
+            farthest = new PIXI.extras.TilingSprite(
+              resources["../../assets/login-sprites.json"].textures["castle2.png"],
+              512, 512
+            );
+            farthest.position.x = 0;
+            farthest.position.y = -130;
+            farthest.scale.y = 1.4;
+            farthest.tilePosition.x = -35;
+            farthest.tilePosition.y = 0;
+            stage.addChild(farthest);
             far.position.x = 0;
-            far.position.y = -30;
-            far.tilePosition.x = 0;
+            far.position.y = -175;
+            far.tilePosition.x = -35;
             far.tilePosition.y = 0;
+            far.scale.y = 1.4;
             stage.addChild(far);
             mid.position.x = 0;
-            mid.position.y = -15;
-            mid.tilePosition.x = 0;
+            mid.position.y = -150;
+            mid.tilePosition.x = -35;
             mid.tilePosition.y = 0;
+            mid.scale.y = 1.2;
             stage.addChild(mid);
             close.position.x = 0;
             close.position.y = 128;
-            close.tilePosition.x = 0;
+            close.tilePosition.x = -35;
             close.tilePosition.y = 0;
             stage.addChild(close);
             requestAnimationFrame(update);
@@ -52,6 +65,7 @@ export default function() {
             close.tilePosition.x -= 0.32;
             mid.tilePosition.x -= 0.128;
             far.tilePosition.x -= 0.064;
+            farthest.tilePosition.x -= 0.032;
 
             renderer.render(stage);
             requestAnimationFrame(update);
